8a1ca1c929764c0bf7f87d4db3766e7e374276c5
GaV1S <GaV1S@yandex.ru>
2021-12-31 14:38:05 +0300

    homework for lesson6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        SearchElem searchElem = new SearchElem();
-        int[] array = searchElem.giveMeArray(100000000);
-        searchElem.helpMeToFindLost(array);
+        int count = 20;
+        int countBalance = 0;
+        for (int i = 0; i < count; i++) {
+            MyTree<Integer, Integer> map = new MyTree<>();
+            while (true) {
+                int value = randomValue();
+                int key = value;
+                map.put(key, value);
+                if (map.height() == 4) {
+                    map.deleteToKey(key);
+                    if (map.isBalance()) {
+                        countBalance++;
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.println("Сбалансированных деревьев: " + countBalance);
+        System.out.println("Общее число деревьев: " + count);
+        System.out.println("Процент сбалансированных деревьев: " + (int)((countBalance / (double) count * 100)) + " %");
+    }
 
+    public static int randomValue() {
+        return (int) ((Math.random() * 51) - 26);
     }
 }
